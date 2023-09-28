25374b5de0a04c4807acf945fb23fe115353792f
ubinix-warun <warun@ubinix.com>
2023-09-28 07:31:23 +0700

    Add tools/jupyter-tf for exec tf deployment.

--- make.sh
+++ make.sh
@@ -17,10 +17,10 @@ export PROJECT_DIR="$(cd "$(dirname "$0")"; pwd)"
 export TF_VAR_project_name=$PROJECT_NAME
 export TF_VAR_region=$AWS_REGION
 
-# log() { echo -e "\e[30;47m ${1^^} \e[0m ${@:2}"; }          # $1 uppercase background white
-# info() { echo -e "\e[48;5;28m ${1^^} \e[0m ${@:2}"; }       # $1 uppercase background green
-# warn() { echo -e "\e[48;5;202m ${1^^} \e[0m ${@:2}" >&2; }  # $1 uppercase background orange
-# error() { echo -e "\e[48;5;196m ${1^^} \e[0m ${@:2}" >&2; } # $1 uppercase background red
+log() { echo -e "\e[30;47m ${1^^} \e[0m ${@:2}"; }          # $1 uppercase background white
+info() { echo -e "\e[48;5;28m ${1^^} \e[0m ${@:2}"; }       # $1 uppercase background green
+warn() { echo -e "\e[48;5;202m ${1^^} \e[0m ${@:2}" >&2; }  # $1 uppercase background orange
+error() { echo -e "\e[48;5;196m ${1^^} \e[0m ${@:2}" >&2; } # $1 uppercase background red
 
 # export functions : https://unix.stackexchange.com/a/22867
 export -f log info warn error
